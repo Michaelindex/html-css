c2b99fa47bd9541f492d038bdf5082ec258b87b7
Michael Santos <mcialbr123@gmail.com>
2023-07-31 21:25:57 -0300

    header finish

--- WithTailwind/tailwind.config.js
+++ WithTailwind/tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azul1: '#0FC2C0',
+        azul2: '#0CABA8',
+        azul3: '#008F8C',
+        azul4: '#015958',
+        azul5: '#023535'
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
 
