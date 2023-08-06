f7fd997cf802615ac7d8e45bb449ebd0f6dd0836
Michael Santos <mcialbr123@gmail.com>
2023-08-06 00:45:02 -0300

    att

---
+++ WithTailwind/src/script.js
@@ -0,0 +1,31 @@
+const words = [
+    'Web-Design',
+    'Desenvolvedor Front-End',
+    'Um pequeno designer',
+    'Analista de Infraestrutura',
+    'Estagiário :(',
+];
+
+const wordElement = document.getElementById('word');
+let currentIndex = 0;
+
+function typeWord(word, index) {
+if (index < word.length) {
+    wordElement.textContent = word.substring(0, index + 1);
+    setTimeout(() => typeWord(word, index + 1), 100);
+} else {
+    setTimeout(() => eraseWord(word, index), 1000);
+}
+}
+
+function eraseWord(word, index) {
+if (index >= 0) {
+    wordElement.textContent = word.substring(0, index);
+    setTimeout(() => eraseWord(word, index - 1), 100);
+} else {
+    currentIndex = (currentIndex + 1) % words.length;
+    setTimeout(() => typeWord(words[currentIndex], 0), 500);
+}
+}
+
+typeWord(words[currentIndex], 0);
