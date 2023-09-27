9a31e54cbb5d1e3a20612bd29ea8e725a918c220
Michael Santos <mcialbr123@gmail.com>
2023-09-27 02:10:37 -0300

    att

---
+++ calc/script.js
@@ -0,0 +1,21 @@
+javascript
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.textContent === 'C') {
+            display.value = '';
+        } else if (button.textContent === '=') {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        } else {
+            display.value += button.textContent;
+        }
+    });
+});
